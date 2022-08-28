3ae4d7832c8337815ce6d9a3c808d70990f3d211
KevinBanegas <kevinandre0504@gmail.com>
2022-08-27 20:53:39 -0600

    Logins Complete

---
+++ src/loginFrames/Cuenta.java
@@ -0,0 +1,22 @@
+package loginFrames;
+
+
+public class Cuenta {
+    private String user;
+    private String contra;
+    public Cuenta(String user, String contra) {
+        this.user = user;
+        this.contra=contra;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+    
+    
+    
+}
